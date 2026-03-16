dc73362009e742a6efb98d04852b32887501766d
Danish <mohddanish47@gmil.com>
2026-03-16 12:07:42 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'login',component:LoginComponent
+  },
+   {
+    path:'',component:LoginComponent
+  },
+  // {
+  //   path:'',component:MainLayoutComponent,
+  //   children:[
+  //     {path:'dashboard',component:DashboardComponent},
+
+  //   ]
+  // }
+];
